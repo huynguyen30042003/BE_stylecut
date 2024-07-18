d647a407221693e07c68602d1afb59bf4f31f85f
HiamNos <ttinh2852@gmail.com>
2024-07-18 21:30:01 +0700

    fixed03

--- routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { searchInAllSalon, searchInSalon } = require('../controllers/searchController');
-const router = express.Router();
-
-router.get('/', searchInAllSalon);
-router.get('/:salonId', searchInSalon);
-
-module.exports = router;
