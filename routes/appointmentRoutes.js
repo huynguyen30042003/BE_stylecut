a312b5df3efce92f1866793e45ebe962918ff0ae
HiamNos <ttinh2852@gmail.com>
2024-07-18 21:33:31 +0700

    fixed04

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const { createAppointment, getAppointments, getAppointmentById, updateAppointment, deleteAppointment } = require('../controllers/appointmentController');
-const { protect, admin, staff, includeOf } = require('../middlewares/authMiddleware');
+const express = require("express");
+const {
+  createAppointment,
+  getAppointments,
+  getAppointmentById,
+  updateAppointment,
+  deleteAppointment,
+} = require("../controllers/appointmentController");
+const {
+  protect,
+  admin,
+  staff,
+  includeOf,
+} = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route('/')
-    .post(protect, createAppointment)
-    .get(protect, includeOf(["Admin", "Staff"]), getAppointments);
+router
+  .route("/")
+  .post(protect, createAppointment)
+  .get(protect, includeOf(["Admin", "Staff"]), getAppointments);
 
-router.route('/:id')
-    .get(protect, getAppointmentById)
-    .put(protect, includeOf(["Admin", "Staff"]), updateAppointment)
-    .delete(protect, admin, deleteAppointment);
+router
+  .route("/:id")
+  .get(protect, getAppointmentById)
+  .put(protect, includeOf(["Admin", "Staff"]), updateAppointment)
+  .delete(protect, admin, deleteAppointment);
 
 module.exports = router;
